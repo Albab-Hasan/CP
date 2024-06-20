fdd296bef5bebb08ba5b5d01efaa38d983697280
Albab Hasan <155961300+Albab-Hasan@users.noreply.github.com>
2024-06-20 19:53:53 +0600

    Update mag_force_btw_2_balls.cpp
    typo

--- mag_force_btw_2_balls.cpp
+++ mag_force_btw_2_balls.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-class solution {
+class Solution {
     public:
         int maxDistance(vector<int>& position, int m) {
             std::sort(position.begin(), position.end());
