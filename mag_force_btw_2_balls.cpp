62d42523886550c3bb1f2533f2f61b7ec58a4cc6
Albab Hasan <155961300+Albab-Hasan@users.noreply.github.com>
2024-06-20 18:29:37 +0600

    Add files via upload

---
+++ mag_force_btw_2_balls.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class solution {
+    public:
+        int maxDistance(vector<int>& position, int m) {
+            std::sort(position.begin(), position.end());
+
+            int l = 1;
+            int r = position.back() - position.front();
+
+            while (l < r) {
+                const int mid = r - (r - l) / 2;
+                if(numballs(position, mid) >= m)
+                    l = mid;
+                else 
+                    r = mid - 1;
+            }
+            return l;
+        }
+    private:
+        int numballs(const vector<int>& position, int force) {
+            int balls = 0;
+
+            int prevPosition = -force;
+            for (const int pos : position)
+                if (pos - prevPosition >= force) {
+                    ++balls;
+                    prevPosition = pos;
+                }
+        return balls;
+    }
+};
